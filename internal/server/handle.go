fc3e3d26d9d477285fe5ae4cab884f742e78a773
stuckinforloop <device.neel@gmail.com>
2024-02-22 20:42:56 +0530

    chore(stage_3): respond to multiple pings

--- internal/server/handle.go
+++ internal/server/handle.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net"
 
 	"github.com/codecrafters-io/redis-starter-go/internal/protocol"
@@ -10,14 +12,24 @@ import (
 func handle(conn net.Conn) error {
 	defer conn.Close()
 
-	// TODO parse request data
+	b := make([]byte, 1024)
+	for {
+		_, err := conn.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return fmt.Errorf("connection read: %w", err)
+		}
 
-	// response for ping command
-	firstByte := protocol.DataTypeToFirstByte[protocol.SimpleString]
-	response := firstByte + "PONG" + "\r\n"
+		// TODO parse request data
 
-	if _, err := conn.Write([]byte(response)); err != nil {
-		return fmt.Errorf("write to conn: %w", err)
+		firstByte := protocol.DataTypeToFirstByte[protocol.SimpleString]
+		response := firstByte + "PONG" + "\r\n"
+
+		if _, err := conn.Write([]byte(response)); err != nil {
+			return fmt.Errorf("write to conn: %w", err)
+		}
 	}
 
 	return nil
